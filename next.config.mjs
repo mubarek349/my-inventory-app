b4dba26d215935ee4b435dc829c770b29f69e310
Mubarek Ahmed <mubarekahmed44@gmail.com>
2024-12-11 12:29:16 +0300

    updating

--- next.config.mjs
+++ next.config.mjs
@@ -12,5 +12,13 @@ const nextConfig = {
             },
         ],
    },
+   async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:3000/api/:path*', // Proxy to Backend
+            },
+        ];
+  },
 };
 export default nextConfig;
