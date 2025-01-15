5b7f3cf244f88b1fb2c2a9dd25df3d42e5525554
Mubarek Ahmed <mubarekahmed44@gmail.com>
2025-01-16 00:20:48 +0300

    updated inventory

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen flex-col">
-       <h2 className="text-3xl mb-4">
+       <h2 className="lg:text-3xl mb-4">
           Inventory Management Software
        </h2>
        <a href="/dashboard/home/overview">View Dashboard</a>
